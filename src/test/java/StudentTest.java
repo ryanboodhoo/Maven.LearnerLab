a95065ea57b86d7a3ab272631a97f34087ad4305
ryanboodhoo <boodho46@gmail.com>
2024-11-03 18:04:43 -0500

    Done up to part 7 with test done

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,13 +1,40 @@
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class StudentTest {
 
     private Student studentSingleton;
+
+    @Test
+    void testImplementation(){
+
+        Student student = new Student("Bob");
+
+        Assertions.assertEquals(true, student instanceof Learner);
+    }
+
+
+
+    @Test
+    void testInheritance () {
+        Student student = new Student("Bob");
+
+        Assertions.assertEquals(true, student instanceof Person);
+
+    }
+
+    @Test
+    void learn () {
+        Student student = new Student("Bob");
+
+        student.learn(8);
+
+        student.learn(8);
+
+        Assertions.assertEquals(16, student.getTotalStudyTime());
+
+
+
+    }
 }
-
-//   @BeforeEach
-//   public void setUp() {
-//       studentSingleton = Student.getInstance();
-//   }
-//}
-
 
